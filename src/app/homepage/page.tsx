89f295000e48c082c95accfb351eacb0517221be
greeshie <grepra17@gmail.com>
2024-09-23 14:27:15 -0500

    Basic Hompage and Nav

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,11 +1,33 @@
+"use client";
+
 import React from 'react';
+import Link from 'next/link';
+import Navbar from '../components/header';
 
-const homepage = () => {
+const HomePage = () => {
   return (
-    <div>
-      homepage
+<div className="relative" id="home">
+    <Navbar />
+    <div className="relative mt-8 px-6 lg:px-8">
+        <div className="relative pt-36 ml-auto">
+            <div className="lg:w-2/3 text-center mx-auto mt-8">
+                <h1 className="text-gray-900 dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl">Tag <span className="text-primary dark:text-white">Line.</span></h1>
+                <p className="mt-8 text-gray-700 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt nam itaque sed eius modi error totam sit illum. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!</p>
+                <div className="mt-8 flex flex-wrap justify-center gap-y-4 gap-x-6">
+                    <Link
+                      href="/auth"
+                      className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+                    >
+                      <span className="relative text-base font-semibold text-white">
+                        Get started
+                      </span>
+                    </Link>
+                </div>
+              </div>
+        </div>
     </div>
+</div>
   );
-}
+};
 
-export default homepage;
+export default HomePage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -11,9 +12,51 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        inherit: colors.inherit,
+        current: colors.current,
+        transparent: colors.transparent,
+        primary: "#000000",
+        secondary: "#ff7e33",
+        info: "#0C63E7",
+        black: colors.black,
+        white: colors.white,
+        slate: colors.slate,
+        gray: {
+          50: "#FAFAFC",
+          100: "#E9E9EC",
+          200: "#C6C8CD",
+          300: "#ACAEB6",
+          400: "#92959F",
+          500: "#777C87",
+          600: "#5D6370",
+          700: "#434959",
+          800: "#293041",
+          900: "#0f172a",
+        },
+        zinc: colors.zinc,
+        neutral: colors.neutral,
+        stone: colors.stone,
+        red: colors.red,
+        orange: colors.orange,
+        amber: colors.amber,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        sky: colors.sky,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        rose: colors.rose,
       },
     },
   },
   plugins: [],
 };
+
 export default config;

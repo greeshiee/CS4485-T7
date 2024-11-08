e35242db2f099db259a4c06100fe44a9f50f5125
fatimakqq <96496894+fatimakqq@users.noreply.github.com>
2024-11-08 13:42:55 -0600

    Removed backend setup

--- backend/data_ingestion/app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class Database(Base):
-    __tablename__ = "databases"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
-    connection_string = Column(String)
--- backend/data_ingestion/run.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import uvicorn
-
-# Add the current directory to the Python path
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)

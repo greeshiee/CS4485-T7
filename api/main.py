475f2e60ff616bd486dacb7fe014834bdf7851ab
ken-myers <ken@kenmyers.io>
2024-12-01 21:35:31 -0600

    change port

--- api/main.py
+++ api/main.py
@@ -16,5 +16,5 @@ app.mount("/", fault_management_app)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
 
